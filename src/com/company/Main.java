4386a7cf8b7ce595920cce9526c225acc001bd0d
Thomas William Komp <twkomp@gmail.com>
2018-06-11 15:22:09 -0400

    added first of the code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
-public class Main {
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    private Scanner input = new Scanner(System.in);
+    private List<String> nameList;
     public static void main(String[] args) {
-	// write your code here
+        Main main = new Main();
+        main.testingLists();
+    }
+    private void testingLists() {
+        nameList = new ArrayList<>();
+        nameList.add("Luke");
+        nameList.add("Timmie");
+        nameList.add("Bob");
+        nameList.add("Bin");
+        nameList.add("hank211");
+        nameList.add("Your Mom");
+        for (int i = 0; i < nameList.size(); i++) {
+            System.out.println((i+1)+" "+nameList.get(i));
+        }
     }
 }
+
